991d00208a1e1da2fa7977e841beb798a5f57537
Baha <bahabayar@hotmail.com>
2021-06-02 17:54:14 +0300

    Add project files.

---
+++ AdSoyadHosgeldin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdSoyadHosgeldin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          Console.WriteLine("İsminizi girin");
+          string name =   Console.ReadLine();
+            Console.WriteLine("Soyadınızı Girin");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba" + name + " " +surname);
+        }
+    }
+}
